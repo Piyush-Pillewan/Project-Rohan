94d9fa92317a25087def8be8322604963c596058
Piyush Pillewan <113883829+Piyush-Pillewan@users.noreply.github.com>
2025-03-03 16:01:26 +0530

    Update output.tf

--- spring-boot-2-rest-service-with-swagger/Project_Terraform/output.tf
+++ spring-boot-2-rest-service-with-swagger/Project_Terraform/output.tf
@@ -1,13 +1,4 @@
-output "instance_ip" {
-    description = "The public IP address of the EC2 instance"
-    value = aws_instance.myec2.public_ip
-}
-
-output "instance_id" {
-    description = "The instance ID of the EC2 instance"
-    value = aws_instance.myec2.instance_id
-}
-
-output "loadbalancerdns" {
-    value = aws_lb.myalb.dns_name
+output "alb_dns_name" {
+  description = "The DNS name of the ALB"
+  value       = aws_lb.web_lb.dns_name
 }
